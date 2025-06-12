94e238057fcddc0e5eda03bc6dd865d22d5e5e72
Prashik Kamble <138839248+prashik536@users.noreply.github.com>
2025-06-13 00:35:56 +0530

    Update Hello.java

--- src/main/java/com/example/Hello.java
+++ src/main/java/com/example/Hello.java
@@ -1,7 +1,11 @@
 package com.example;
 
 public class Hello {
+    public String greet() {
+        return "Hello, Jenkins!";
+    }
+
     public static void main(String[] args) {
-        System.out.println("✅ Hello from Jenkins Test Project!");
+        System.out.println(new Hello().greet());
     }
 }

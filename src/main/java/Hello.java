45db6419c8f3008ea2c412d376ca98044e854e47
Prashik Kamble <138839248+prashik536@users.noreply.github.com>
2025-06-13 00:30:33 +0530

    Delete src/main/java directory

--- src/main/java/Hello.java
+++
@@ -1,5 +0,0 @@
-public class Hello {
-    public String greet() {
-        return "Hello from Jenkins Agent!";
-    }
-}
--- src/main/java/com/example/Hello.java
+++
@@ -1,7 +0,0 @@
-package com.example;
-
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("✅ Hello from Jenkins Test Project!");
-    }
-}
